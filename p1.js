e9422291df3f1456e90a2d19c9db253512c2a05c
Daniel Adamczak <daniel.marcin.a@gmail.com>
2022-10-04 11:21:33 +0200

    p1 finish - js update

--- p1.js
+++ p1.js
@@ -1,9 +1,18 @@
 const search = document.querySelector(".search");
 const li = document.querySelectorAll("li");
 const searchEngine = e => {
-    const text = e.target.value;
+    const text = e.target.value.toLowerCase();
     console.log(text);
+    console.log(search.textContent);
     li.forEach(el => {
+ 
+        if(el.textContent.toLowerCase().indexOf(text) !== -1) {
+            el.style.display ='block';
+            
+        }
+        else {
+            el.style.display ='none';
+        }
         console.log(el);
     })
 
